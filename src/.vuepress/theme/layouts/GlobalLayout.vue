8c21deada7ea30c797e60765a42fa9b8114c8733
elenh <yisiwings@163.com>
2020-03-19 19:04:24 +0800

    fix: idou-toolbar, idou-sidebar position

--- src/.vuepress/theme/layouts/GlobalLayout.vue
+++ src/.vuepress/theme/layouts/GlobalLayout.vue
@@ -4,13 +4,13 @@
       <parent-layout>
         <template #page-top v-if="showPageTop">
           <div class="global-layout__page-top">
-            <idou-toolbar class="global-layout__page-top__tool-bar" />
             <component :is="layout" :key="layout" />
-            <idou-sidebar class="global-layout__page-top__side-bar" />
           </div>
         </template>
         <template #page-bottom></template>
       </parent-layout>
+      <idou-toolbar class="global-layout__page__tool-bar" />
+      <idou-sidebar class="global-layout__page__side-bar" />
     </div>
     <idou-footer />
   </div>
@@ -51,24 +51,27 @@ export default {
   
   .global-layout__page {
     min-height: 94vh;
+    max-width: 960px;
+    margin: 0 auto;
+    position: relative;
+
+    &__side-bar {
+      position: absolute;
+      top: 75px;
+      right: -17.5rem;
+    }
+
+    &__tool-bar {
+      position: absolute;
+      top: 134px;
+      left: -4.2rem;
+    }
+
     &-top {
       max-width: 960px;
-      position: relative;
       margin: 15px auto;
       box-sizing: border-box;
       background-color: #fff;
-
-      &__side-bar {
-        position: absolute;
-        top: 0px;
-        right: -17.5rem;
-      }
-
-      &__tool-bar {
-        position: absolute;
-        top: 70px;
-        left: -4.2rem;
-      }
     }
   }
 
