0dfd1abf7aec5de362169cd095d48d63b0ab02c4
elenh <yisiwings@163.com>
2020-03-20 17:44:57 +0800

    add: idou.config.js file

---
+++ idou.config.js
@@ -0,0 +1,17 @@
+// themeConfig
+module.exports = {
+  nav: [
+    { text: '首页', link: '/' },
+    { text: '指南', link: '/guide/' },
+    { text: '关于', link: '/about/' },
+    { text: '标签', link: '/tags/' },
+    { text: '分类', link: '/categories/' },
+    { text: '归档', link: '/archive/' },
+    { text: '作者博客', link: 'http://www.hongwenqing.com' },
+  ],
+  displayAllHeaders: true,
+  activeHeaderLinks: true,
+  sidebar: false,
+  smoothScroll: true,
+  lastUpdated: '最后更新于', // string | boolean
+}
--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,24 +1,11 @@
+const themeConfig = require('../../idou.config.js')
+
 module.exports = {
   base: '/idou/',
   title: 'idou',
   dest: './dist',
   description: 'A theme for VuePress',
-  themeConfig: {
-    nav: [
-      { text: '首页', link: '/' },
-      { text: '指南', link: '/guide/' },
-      { text: '关于', link: '/about/' },
-      { text: '标签', link: '/tags/' },
-      { text: '分类', link: '/categories/' },
-      { text: '归档', link: '/archive/' },
-      { text: '作者博客', link: 'http://www.hongwenqing.com' },
-    ],
-    displayAllHeaders: true,
-    activeHeaderLinks: true,
-    sidebar: false,
-    smoothScroll: true,
-    lastUpdated: '最后更新于', // string | boolean
-  },
+  themeConfig,
   plugins: [
     [require('./plugins/post-menu.js')],
     '@vuepress/back-to-top'
