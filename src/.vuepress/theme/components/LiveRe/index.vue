8f8f4ffd6408304cb9b2c48d0c08f9f4dd4ba502
elenh <yisiwings@163.com>
2020-04-03 11:15:34 +0800

    fix: 来必力 激活脚本报错问题。

--- src/.vuepress/theme/components/LiveRe/index.vue
+++ src/.vuepress/theme/components/LiveRe/index.vue
@@ -5,24 +5,9 @@
 -->
 <template>
   <div class="live-re-container">
-    <!-- 来必力City版安装代码 -->
     <div id="lv-container" :data-id="type" :data-uid="uid">
-    <script type="text/javascript">
-      (function(d, s) {
-          var j, e = d.getElementsByTagName(s)[0];
-
-          if (typeof LivereTower === 'function') { return; }
-
-          j = d.createElement(s);
-          j.src = 'https://cdn-city.livere.com/js/embed.dist.js';
-          j.async = true;
-
-          e.parentNode.insertBefore(j, e);
-      })(document, 'script');
-    </script>
-    <noscript>为正常使用来必力评论功能请激活JavaScript</noscript>
+      <noscript>为正常使用来必力评论功能请激活JavaScript</noscript>
     </div>
-    <!-- City版安装代码已完成 -->
   </div>
 </template>
 
@@ -44,6 +29,23 @@ export default {
     },
     uid() {
       return this.service.LiveRe.uid
+    }
+  },
+  mounted() {
+    this.init()
+  },
+  methods: {
+    init(s = 'script') {
+      const d = document
+      let j, e = d.getElementsByTagName(s)[0];
+
+      if (typeof LivereTower === 'function') { return; }
+
+      j = d.createElement(s);
+      j.src = 'https://cdn-city.livere.com/js/embed.dist.js';
+      j.async = true;
+
+      e.parentNode.insertBefore(j, e);
     }
   }
 }
