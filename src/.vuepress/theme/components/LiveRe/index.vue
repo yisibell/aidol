71ab5cd9a60149c9bf2c8e8a6ca5eb62c634457f
elenh <yisiwings@163.com>
2020-04-03 17:45:21 +0800

    fix: livere 加载问题。

--- src/.vuepress/theme/components/LiveRe/index.vue
+++ src/.vuepress/theme/components/LiveRe/index.vue
@@ -39,13 +39,18 @@ export default {
       const d = document
       let j, e = d.getElementsByTagName(s)[0];
 
-      if (typeof LivereTower === 'function') { return; }
+      if (typeof LivereTower === 'function') return
 
-      j = d.createElement(s);
-      j.src = 'https://cdn-city.livere.com/js/embed.dist.js';
-      j.async = true;
-
-      e.parentNode.insertBefore(j, e);
+      j = d.createElement(s)
+      j.src = 'https://cdn-city.livere.com/js/embed.dist.js'
+      j.async = true
+      
+      if (e) {
+        e.parentNode.insertBefore(j, e)
+      } else {
+        d.body.insertBefore(j, d.body.firstChild)
+      }
+      
     }
   }
 }
