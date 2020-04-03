975682c8f35af3506a886e93cbd335232386398e
elenh <yisiwings@163.com>
2020-04-03 17:33:54 +0800

    fix: busuanzi 重复注册问题。

--- src/.vuepress/theme/service/busuanzi.js
+++ src/.vuepress/theme/service/busuanzi.js
@@ -7,8 +7,13 @@ const install = () => {
   const d = document
   const s = d.createElement('script')
   const body = d.querySelector('body')
+  const idouBusuanzi = d.querySelector('#idou-busuanzi-script')
+
+  s.id = 'idou-busuanzi-script'
   s.async = true
   s.src = '//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js'
+
+  if (idouBusuanzi) idouBusuanzi.remove()
   body.append(s)
 }
 
