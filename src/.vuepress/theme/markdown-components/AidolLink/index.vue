624100da4381f57d4fa89433d7193b56fc087828
elenh <yisiwings@163.com>
2020-04-29 14:15:03 +0800

    add: aidol-link.

---
+++ src/.vuepress/theme/markdown-components/AidolLink/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-link v-if="type" v-bind="$props"><slot name="default"></slot></el-link>
+  <a v-else v-bind="$props"><slot name="default"></slot></a>
+</template>
+
+<script>
+export default {
+  name: 'AidolLink',
+  props: {
+    type: {
+      type: String,
+      default: ''
+    },
+    underline: {
+      type: Boolean,
+      default: false
+    },
+    href: {
+      type: String,
+      default: ''
+    },
+    icon: {
+      type: String,
+      default: ''
+    },
+    target: {
+      type: String,
+      default: '_blank'
+    }
+  }
+}
+</script>
