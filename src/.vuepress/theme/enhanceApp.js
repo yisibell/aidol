47a63be021f26aa7ecb60b08582d652962e23de5
wenqing <wenqing@kerrylan.com>
2023-04-19 17:22:00 +0800

    fix: vue-symbol-icon error

--- src/.vuepress/theme/enhanceApp.js
+++ src/.vuepress/theme/enhanceApp.js
@@ -1,25 +1,31 @@
 import ElementUI from 'element-ui'
-import Icons from './icons'
 import CustomUI from './components'
 import Layer from './utils/layer'
 import MarkdownComponents from './markdown-components'
+import SvgIcon from 'vue-symbol-icon'
 
+import './icons'
 import './styles/main.scss'
 
 export default ({
   Vue, // VuePress 正在使用的 Vue 构造函数
   options, // 附加到根实例的一些选项
   router, // 当前应用的路由实例
-  siteData // 站点元数据
+  siteData, // 站点元数据
+  isServer // 当前应用配置是处于 服务端渲染 或 客户端
 }) => {
   // ...做一些其他的应用级别的优化
   Vue.use(ElementUI)
+
   // svg图标
-  Vue.use(Icons)
+  Vue.use(SvgIcon)
+
   // 自定义UI
   Vue.use(CustomUI)
+
   // 弹出层
   Vue.prototype.$layer = Layer
+
   // markdown 文件专用组件
   Vue.use(MarkdownComponents)
 }
